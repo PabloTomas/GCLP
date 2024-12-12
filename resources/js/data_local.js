a6667b6d49301703bb369b8d83788d6c751bf407
PabloTomas <patomas@geocities.com>
2024-12-12 17:24:04 +0000

    wrong path ñapa

--- resources/js/data_local.js
+++ resources/js/data_local.js
@@ -18,7 +18,7 @@ function UrlExists(url) {
 function getJsonFiles(){
     let i=1;
     let exists=true;
-    let file = "./resource/data/v#.json";
+    let file = "/GCLP/resource/data/v#.json";
     let files = new Array();
     while (exists){
         let f = file.replace("#",i);
